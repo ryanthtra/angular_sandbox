df08fba3c7bb7c2463325b3d2c21a89222cfea08
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2016-05-05 03:39:41 -0700

    angular_sanbox parking_01 - before creating custom directives

---
+++ parking/js/controllers.js
@@ -0,0 +1,21 @@
+parking.controller("parkingCtrl", function($scope) 
+{
+	$scope.appTitle = "Parking App";
+	
+	$scope.cars = [];
+	
+	$scope.colors = [
+		"White",
+		"Black",
+		"Blue",
+		"Red",
+		"Silver"
+	];
+	
+	$scope.park = function(car) 
+	{
+		car.entrance = new Date();
+		$scope.cars.push(car);
+		delete $scope.car;
+	};
+});
